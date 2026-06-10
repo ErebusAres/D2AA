27759782f58f6c53cdc924302909fc84f990c83e
Chris Brownlee <cbrownlee@plumcreeket.onmicrosoft.com>
2026-06-10 15:17:07 -0500

    Serve built app from GitHub Pages root

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,18 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/D2AA/'
+  base: '/D2AA/',
+  build: {
+    rollupOptions: {
+      input: {
+        index: 'index.html',
+        app: 'src/main.tsx'
+      },
+      output: {
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name][extname]'
+      }
+    }
+  }
 });
